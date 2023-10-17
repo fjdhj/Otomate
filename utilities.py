4bb5b6f2a2d48907c6d4ddece028f1e1d07ea0f2
hadjuse <rabearimananahadjpro@gmail.com>
2023-10-17 21:08:33 +0200

    create function init_graph

--- utilities.py
+++ utilities.py
@@ -1,8 +1,22 @@
 import pandas as pd
+import numpy as np
 
+def init_graph(file)->list:
+    """This function enable us to generate 
+    matrix that contains the events and links from
+    the CSV File.
 
-automate = pd.read_csv('Sample/default.csv', sep=';')
-dimension=automate.shape
-graphe = automate.iloc[1:dimension[0],1:dimension[1]-2]
-print(graphe.head())
-    
+    Args:
+        file (_type_): _description_
+
+    Returns:
+        list: _description_
+    """
+    automate = pd.read_csv(file, sep=';')
+    dimension=automate.shape
+    graph = automate.iloc[0:dimension[0],1:dimension[1]-2]
+    graph_to_list=[graph.loc[i, :].values.flatten().tolist() for i in range(dimension[0])]
+    return graph_to_list
+
+init_graph('Sample/default.csv')
+        
