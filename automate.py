bca645df68a0a996b0dd534734e6f6957aac2a26
imadismad <i.bouaamri17@gmail.com>
2023-11-13 21:07:11 +0100

    added mirror function

--- automate.py
+++ automate.py
@@ -73,16 +73,42 @@ Final_states: {self.final_states}
     def display_transition(self):
         print(self.transitions)
 
-    def add_transition(self, initial_state, transition="c", final_state="sale boulot"):
-        if not (transition in self.transitions):
-            raise ValueError(
-                "La transition entrée n'est pas dans la colonne veuillez saisir une autre")
+    # ... (other methods)
+    # ... (other methods)
+
+    # ... (existing methods)
+
+    # ... (other existing methods)
+
+    def add_transition(self, initial_state, transition_symbol, final_state):
+        # Find the index of the initial state and transition symbol
         index_state = self.all_states.index(initial_state)
-        index_transition = self.transitions.index(transition)
-        if (self.matrix[index_state][index_transition][0] == "nan"):
-            del self.matrix[index_state][index_transition][0]
+        # +1 to account for the state name in self.matrix
+        index_transition = self.transitions.index(transition_symbol) + 1
+
+        # Get the current transition for the state and symbol
+        current_transition = self.matrix[index_state][index_transition]
+
+        # Handle the case of no existing transition (nan)
+        if current_transition == 'nan':
+            self.matrix[index_state][index_transition] = final_state
         else:
-            self.matrix[index_state][index_transition].append(final_state)
+            # If there is an existing transition, update it
+            # Check if the current transition is a single state or a list of states
+            if ',' in current_transition:
+                # Split the current transition into a list, add the new state if it's not already there, and join back
+                current_transitions = current_transition.split(',')
+                if final_state not in current_transitions:
+                    current_transitions.append(final_state)
+                self.matrix[index_state][index_transition] = ','.join(
+                    current_transitions)
+            else:
+                # If the current transition is a single state, create a list with the new state
+                if current_transition != final_state:
+                    self.matrix[index_state][index_transition] = ','.join(
+                        [current_transition, final_state])
+
+    # ... (rest of the automate class)
 
     def display_matrix(self):
         pprint(self.matrix)
@@ -114,8 +140,6 @@ Final_states: {self.final_states}
         # Inverting final states: If a state is final (1), it becomes non-final (0) and vice versa.
         self.final_states = [1 if state ==
                              0 else 0 for state in self.final_states]
-
-    import pandas as pd
 
     def edit_csv(self, filename):
         # Create a DataFrame to hold the CSV data
@@ -150,14 +174,47 @@ Final_states: {self.final_states}
         # Write to CSV using pandas, with the header
         df.to_csv(f"Sample/{filename}.csv", index=False, header=True, sep=';')
 
+    # ... (existing methods)
+
+    def mirror(self):
+        # Initialize a mirrored matrix
+        mirrored_matrix = [[state] + ['nan' for _ in self.transitions]
+                           for state in self.all_states]
+
+        # Reverse the transitions
+        for state_index, state in enumerate(self.all_states):
+            for trans_index, transition in enumerate(self.transitions):
+                transition_targets = self.matrix[state_index][trans_index + 1]
+                if isinstance(transition_targets, float):
+                    # Convert the float to a string before splitting
+                    transition_targets = str(transition_targets)
+                if transition_targets != 'nan':
+                    for target_state in transition_targets.split(','):
+                        if target_state:
+                            # Add a reversed transition in the mirrored matrix
+                            target_index = self.all_states.index(target_state)
+                            mirrored_matrix[target_index][trans_index + 1] = state
+
+        # Swap the initial and final states
+        self.initial_states, self.final_states = self.final_states[:], self.initial_states[:]
+
+        # Update the current object with the mirrored matrix and states
+        self.matrix = mirrored_matrix
+
 
 automate1 = automate(sample_event, sample_state)
 automate1.display_matrix()
 
+automate1.mirror()
+
 automate1.display_matrix()
-print(sample_event)
 automate1.edit_csv("test")
 
+# mirror_automaton = automate1.mirror()
+# mirror_automaton.display_matrix()
+
+# Now, you can edit and save the mirrored automaton if needed
+# mirror_automaton.edit_csv("test")
 
 """
 automate1.split_states()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,30 @@
+    def mirror(self):
+        # Create a new mirrored automaton with reversed transitions
+        mirrored_matrix = [list(row) for row in self.matrix]
 
-    def edit_csv(self, filename):
+        # Reverse the transitions in the mirrored matrix
+        for state_index, state in enumerate(self.all_states):
+            for trans_index, transition in enumerate(self.transitions):
+                transition_targets = self.matrix[state_index][trans_index + 1]
+                if isinstance(transition_targets, float):
+                    # Convert the float to a string before splitting
+                    transition_targets = str(transition_targets)
+                if transition_targets != 'nan':
+                    for target_state in transition_targets.split(','):
+                        if target_state:
+                            # Update the mirrored matrix with reversed transitions
+                            target_index = self.all_states.index(target_state)
+                            mirrored_matrix[state_index][trans_index +
+                                                         1] = target_state
+
+        # Reverse the initial and final states in the mirrored automaton
+        mirrored_initial_states = [1 if state ==
+                                   0 else 0 for state in self.final_states]
+        mirrored_final_states = [1 if state ==
+                                 0 else 0 for state in self.initial_states]
+
+        # Create a new mirrored automaton with the mirrored matrix and states
+        mirrored_automaton = automate(mirrored_matrix, [
+            mirrored_initial_states, mirrored_final_states], self.transitions)
+
+        return mirrored_automaton
