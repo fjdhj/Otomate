27dd21a8fb7f21edb17cf31299eb8b8e5558748e
imadismad <i.bouaamri17@gmail.com>
2023-11-13 18:47:11 +0100

    edit_csv + complement

--- utilities.py
+++ utilities.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 
-#modifier init_graph et init_statestypes par rapport au fichier test.csv
-def init_graph(file)->list:
+# modifier init_graph et init_statestypes par rapport au fichier test.csv
+
+
+def init_graph(file) -> list:
     """This function enable us to generate 
     matrix that contains the events and links from
     the CSV File.
@@ -14,12 +16,14 @@ def init_graph(file)->list:
         list: 2D arrays which contains automaton graph
     # """
     automate = pd.read_csv(file, sep=';')
-    dimension: tuple=automate.shape
-    graph = automate.iloc[0 :dimension[0],0 : dimension[1]-2]
-    graph_to_list=[graph.loc[i,:].values.tolist() for i in range(dimension[0])]        
+    dimension: tuple = automate.shape
+    graph = automate.iloc[0:dimension[0], 0: dimension[1]-2]
+    graph_to_list = [graph.loc[i, :].values.tolist()
+                     for i in range(dimension[0])]
     return graph_to_list
 
-def init_statestypes(file)->list:
+
+def init_statestypes(file) -> list:
     """This function gets the types of the
     states (initial or final).
 
@@ -30,12 +34,13 @@ def init_statestypes(file)->list:
         list: list of the states's types
     # """
     automate = pd.read_csv(file, sep=';')
-    dimension: tuple=automate.shape
-    states = automate.iloc[0 :dimension[0],dimension[1]-2 : dimension[1]]
-    states_to_list=[states.loc[:,i].values.tolist() for i in ['EI', 'EF']]
-    print(states_to_list)
+    dimension: tuple = automate.shape
+    states = automate.iloc[0:dimension[0], dimension[1]-2: dimension[1]]
+    states_to_list = [states.loc[:, i].values.tolist() for i in ['EI', 'EF']]
+    # print(states_to_list)
     return states_to_list
 
-def transitions(file)->list:
-    automate=pd.read_csv(file,sep=';')
+
+def transitions(file) -> list:
+    automate = pd.read_csv(file, sep=';')
     return list(automate.columns)[1:automate.shape[1]-2]
