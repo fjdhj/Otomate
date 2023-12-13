4e77680524b9e4e308f5fe867d48e0cf301a71ea
imadismad <104365795+imadismad@users.noreply.github.com>
2023-12-13 10:28:04 +0100

    added a unit test for deterministing method

---
+++ unit_test_determinist.py
@@ -0,0 +1,29 @@
+import unittest
+from automate import automate  # Import the automate class
+
+class TestAutomateDeterminist(unittest.TestCase):
+
+    def setUp(self):
+        print("\nSetting up for a new test...")
+
+    def tearDown(self):
+        print("Test completed.")
+
+
+    def create_automaton_from_csv(self, file_name):
+        print(f"Creating automaton from {file_name}...")
+        return automate(file_name)
+
+    def test_deter(self):
+        print("Testing Automaton Non-Deterministic to Finite Deterministic (AND to AFD)...")
+        automaton = self.create_automaton_from_csv("otomate5.csv")
+        #automaton = self.create_automaton_from_csv("UNITTESTS/automaton_nondeterminist.csv")
+        automaton.display_matrix()
+        automaton.AND_to_AFD()
+        print("\n")
+        automaton.display_matrix()
+        print("\n")
+        
+# Run the tests
+if __name__ == '__main__':
+    unittest.main()
