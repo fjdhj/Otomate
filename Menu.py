f0b05f8ed24c7c42eaef2c1cd3f7658677faa732
imadismad <i.bouaamri17@gmail.com>
2023-12-15 15:16:46 +0100

    added the manual in the menu

--- Menu.py
+++ Menu.py
@@ -1,5 +1,7 @@
 from automate import automate
 from convertCsvPng import draw_and_save_automaton
+from convertJffCsv import jff_to_csv
+
 import os
 import webbrowser
 
@@ -51,6 +53,140 @@ def modifier_automate(actual_auto:automate):
         else:
             print("Choix invalide. Veuillez réessayer.\n")
 
+def display_manual():
+    """
+    Display the user manual for the Automata Management System.
+    """
+    manual = """
+    ############################## User Manual ##############################
+
+    Welcome to the Automata Management System. Below is a guide to using each feature of the system.
+
+    1. Import Automaton:
+       - Import an automaton from a file.
+       - Input: CSV file name.
+       - Output: Automaton loaded into an available slot.
+       - Example: Input "automaton.csv" to import.
+
+    2. Export Automaton:
+       - Export an automaton to a file.
+       - Input: Slot number and export file name.
+       - Output: Automaton from the specified slot exported to the file.
+       - Example: File name "export.csv" to export.
+
+    3. Create Automaton:
+       - Create a new automaton.
+       - Input: Automaton details (states, transitions).
+       - Output: New automaton stored in a slot.
+       - Example: Provide state and transition details to create.
+
+    4. Modify Automaton:
+       - Modify an existing automaton.
+       - Input: Slot number and modification details.
+       - Output: Updated automaton in the slot.
+       - Example: Input slot "3" to modify Automaton in Slot 3.
+
+    5. Delete Automaton:
+       - Delete an automaton from a slot.
+       - Input: Slot number.
+       - Output: Automaton in the slot is deleted.
+       - Example: Input slot "4" to delete Automaton in Slot 4.
+
+    6. Process a Word:
+       - Check if a word is accepted by an automaton.
+       - Input: Slot number and word.
+       - Output: Result of word processing.
+       - Example: Input slot "1" and word "abc" to test.
+
+    7. Complete Automaton:
+       - Check if an automaton is complete and complete it if needed.
+       - Input: Slot number.
+       - Output: Completeness status and option to complete.
+       - Example: Input slot "5" to check and complete Automaton in Slot 5.
+
+    8. Deterministic Automaton:
+       - Check and convert an automaton to a deterministic form.
+       - Input: Slot number.
+       - Output: Information on whether the automaton is deterministic; option to convert it if not.
+       - Example: Input slot "6" to check and convert Automaton in Slot 6 if necessary.
+
+    9. Mirror Automaton:
+       - Create a mirror version of the automaton.
+       - Input: Slot number.
+       - Output: Mirrored automaton in the same slot.
+       - Example: Input slot "7" to mirror Automaton in Slot 7.
+
+    10. Complement:
+        - Generate the complement of an automaton.
+        - Input: Slot number.
+        - Output: Complement automaton in the same slot.
+        - Example: Input slot "8" to complement Automaton in Slot 8.
+
+    11. Product:
+        - Create a product of two automata.
+        - Input: Slot numbers of the two automata.
+        - Output: Product automaton stored in a new slot.
+        - Example: Input slots "2" and "3" to create a product of Automata in Slots 2 and 3.
+
+    12. Concatenation:
+        - Concatenate two automata.
+        - Input: Slot numbers of the two automata.
+        - Output: Concatenated automaton stored in a new slot.
+        - Example: Input slots "4" and "5" to concatenate Automata in Slots 4 and 5.
+
+    13. Extract Regular Expressions:
+        - Extract regular expressions from an automaton.
+        - Input: Slot number.
+        - Output: Regular expressions corresponding to the automaton.
+        - Example: Input slot "9" to extract expressions from Automaton in Slot 9.
+
+    14. Find Language:
+        - Determine the language of an automaton.
+        - Input: Slot number.
+        - Output: Description of the language recognized by the automaton.
+        - Example: Input slot "10" to find the language of Automaton in Slot 10.
+
+    15. Equivalence between 2 Automata:
+        - Check the equivalence of two automata.
+        - Input: Slot numbers of the two automata.
+        - Output: Result of equivalence check.
+        - Example: Input slots "1" and "2" to check equivalence between Automata in Slots 1 and 2.
+
+    16. Prune Automaton:
+        - Prune an automaton to remove unnecessary states.
+        - Input: Slot number.
+        - Output: Pruned automaton in the same slot.
+        - Example: Input slot "11" to prune Automaton in Slot 11.
+
+    17. Minimize Automaton:
+        - Minimize an automaton.
+        - Input: Slot number.
+        - Output: Minimized automaton in the same slot.
+        - Example: Input slot "12" to minimize Automaton in Slot 12.
+
+    18. Visualize Automaton:
+        - Visualize and export the image of an automaton.
+        - Input: Slot number.
+        - Output: Visualization of the automaton and export as an image file.
+        - Example: Input slot "13" to visualize and export Automaton in Slot 13.
+
+    19. Convert .JFF to .CSV:
+        - Convert a .JFF file to a .CSV file format.
+        - Input: .JFF file name and .CSV file name.
+        - Output: Converted .CSV file.
+        - Example: Input "automaton.jff" and "automaton.csv" for conversion.
+
+    20. Manual:
+        - Displays this user manual.
+
+    21. Exit Program:
+        - Use this option to exit the program. Ensure to export any automata you wish to save.
+
+    For more information or issues, refer to the system documentation or contact support.
+
+    ############################################################################
+    """
+    print(manual)
 
 
 ##########################################################################################
@@ -105,9 +241,9 @@ while True:
     print("16. Emonder automate")
     print("17. Rendre un automate minimal")
     print("18. Visualiser un automate")
-    print("19. Manuel")
-    print("20. Quitter le programme\n")
-    
+    print("19. Convertir .JFF en .CSV")
+    print("20. Manuel")
+    print("21. Quitter le programme\n")
     choix = input("Choisissez une action : ")
     print("\n")
 
@@ -355,15 +491,22 @@ while True:
         else:
             print("Aucun automate dans ce slot.\n")
         
-    # ### MANUEL ###########################################################
-            
-    # elif choix == "19":
-    #     #afficher_manuel()
-        
-    
+    #### JFF to CSV ###########################################################
+
+    elif choix == "19":
+        print("Vous voulez convertir un fichier .jff en .csv \n")
+        jff_filename = input("Entrez le nom du fichier .jff  : ")
+        csv_filename = input("Entrez le nom du fichier .csv  : ")
+        jff_to_csv(jff_filename, csv_filename)
+
+    #### MANUEL ###########################################################
+
+    elif choix == "20":
+        display_manual()
+
     # ### EXIT #############################################################
         
-    elif choix == "20":
+    elif choix == "21":
         qu = input("Après avoir quitté, vos automates seront supprimés des slots.\nAvez-vous bien exporté tous les automates que vous vouliez ? (0/N) \n")
         if qu in ["O","OUI","Oui","o","oui","Yes","Y","y","yes","YES"]:
             print("Merci et à bientôt !")
