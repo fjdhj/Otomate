91697216c3e68a75ef5088c27d8b5c1bfaa7148d
Wanbl <106831185+Wanbl@users.noreply.github.com>
2024-01-14 13:53:17 +0100

    Update automate.py
    suppr debug line

--- automate.py
+++ automate.py
@@ -251,7 +251,6 @@ Final_states: {self.final_states}
             raise ValueError("The transition entered is not in the column please enter another")
         index_state=self.all_states.index(initial_state)
         index_transition=self.transitions.index(transition)
-        print(self.matrix[index_state][index_transition+1])
         if(final_state in self.matrix[index_state][index_transition+1].split(",")):
             print("The transition already exists")
         elif(self.matrix[index_state][index_transition+1] == "nan"):
